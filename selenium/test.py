fd2eda77f54da0a9c420f04724c6e90256bf0216
Bas Zoetekouw <bas.zoetekouw@surfnet.nl>
2019-03-23 14:19:39 +0100

    small refactoring

--- selenium/test.py
+++ selenium/test.py
@@ -1,18 +1,20 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
-#from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.keys import Keys
 from xvfbwrapper import Xvfb
 from time import sleep
 
 
-class Selenium(unittest.TestCase):
+# generic class for SCZ tests
+# includes selenium setup and generic helper methods for common functionality
+class SCZTest(unittest.TestCase):
     def __init__(self, method_name='runTest'):
         self.base = 'test.scz.lab.surf.nl'
-        self.test_idp_entityid = "https://idp-test.test.scz.lab.surf.nl/saml/saml2/idp/metadata.php"
+        self.test_idp_entityid = f'https://idp-test.{self.base}/saml/saml2/idp/metadata.php'
         self.test_idp_name = "SCZ Test IdP"
         self.test_idp_admin = {"user": "baas", "pass": "baas"}
-        super(Selenium, self).__init__(method_name)
+        super(SCZTest, self).__init__(method_name)
 
     def setUp(self):
         self.driver = webdriver.Firefox()
@@ -23,8 +25,8 @@ class Selenium(unittest.TestCase):
         if self.driver.session_id:
             self.driver.quit()
 
-    # shortcut to select the idp with the specified entityid in the pyFf decivery screen
-    def _wayf_select_idp(self, entityid, search_text=None, idp_name = None):
+    # shortcut method to select the idp with the specified entityid in the pyFF decovery screen
+    def scz_wayf_select_idp(self, entityid, search_text=None, idp_name=None):
         d = self.driver
 
         # if no search text was specified, we simply search for the entityid
@@ -40,8 +42,7 @@ class Selenium(unittest.TestCase):
         sleep(1.0)
 
         # find correct idp item
-        idp_selector = d.find_element_by_xpath(
-            f'//*[@id="ds-search-list"]/div[@data-href="{entityid}"]//*/h5')
+        idp_selector = d.find_element_by_xpath(f'//*[@id="ds-search-list"]/div[@data-href="{entityid}"]//*/h5')
 
         # extra check for the correct idp name, if requested
         if idp_name is not None:
@@ -50,7 +51,7 @@ class Selenium(unittest.TestCase):
         idp_selector.click()
 
     # shortcut method to login to the Test IdP with the provided credentials
-    def _test_idp_login(self, username, password):
+    def scz_test_idp_login(self, username, password):
         d = self.driver
 
         self.assertEqual("Enter your username and password", self.driver.title)
@@ -58,6 +59,9 @@ class Selenium(unittest.TestCase):
         d.find_element_by_id('password').send_keys(password)
         d.find_element_by_xpath('//*[@id="submit"]//*/button').click()
 
+
+# test COmanage
+class SCZTestCOmanage(SCZTest):
     def test_comanage_login_testidp(self):
         d = self.driver
 
@@ -69,24 +73,22 @@ class Selenium(unittest.TestCase):
         login_button.click()
 
         # select Test IdP
-        self._wayf_select_idp(self.test_idp_entityid, search_text='SCZ', idp_name=self.test_idp_name)
+        self.scz_wayf_select_idp(self.test_idp_entityid, search_text='SCZ', idp_name=self.test_idp_name)
 
         # login
-        self._test_idp_login(self.test_idp_admin['user'], self.test_idp_admin['pass'])
+        self.scz_test_idp_login(self.test_idp_admin['user'], self.test_idp_admin['pass'])
 
         # check that we are in the correct COmanage
         self.assertEqual("COmanage Registry", d.find_element_by_xpath('//*[@id="collaborationTitle"]').text)
         self.assertEqual(f'https://comanage.{self.base}/registry/', d.current_url)
         # check that we have a "Platform" manu (i.e., that we are admin)
         self.assertEqual('Platform',
-            d.find_element_by_xpath('//li[@class="platformMenu"]//*/*[@class="menuTitle"]').text)
+                         d.find_element_by_xpath('//li[@class="platformMenu"]//*/*[@class="menuTitle"]').text)
 
-        sleep(2)
         self.test_result = 'pass'
 
 
 if __name__ == '__main__':
     display = Xvfb(width=1280, height=740, colordepth=16)
-    #display.start()
-    unittest.main()
-    #display.stop()
+    # display.start()
+    unittest.main()  # display.stop()
